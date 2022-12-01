b2290c68b87a317eb505bff07b39308e6dea0318
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-01 20:37:35 +0200

    Created navigation

--- ExamPrepGamePlay/src/views/nav.js
+++ ExamPrepGamePlay/src/views/nav.js
@@ -5,32 +5,27 @@ import { getUserData } from "../util.js";
 const nav = document.querySelector("nav");
 
 const navTemplate = (hasUser) => html` 
- <img src="./images/headphones.png">
-                <a href="/">Home</a>
-                <ul>
-                    <!--All user-->
-                    <li><a href="/catalog">Catalog</a></li>
-                    <li><a href="/search">Search</a></li>
-                    <!--Only guest-->
-                    ${hasUser ?
-                    html`<li><a href="/create">Create Album</a></li>
-                    <li><a @click=${onLogout} href="javascript:void(0)">Logout</a></li>`: 
-                    html`<li><a href="/login">Login</a></li>
-                    <li><a href="/register">Register</a></li>`}
-                    
-                    <!--Only user-->
-                    
-                </ul>`;
+<a href="#">All games</a>
+  <!-- Logged-in users -->
+  ${hasUser ? html`
+  <div id="user">
+    <a href="#">Create Game</a>
+    <a href="#">Logout</a>
+  </div>`
+  : html`
+  <div id="guest">
+    <a href="#">Login</a>
+    <a href="#">Register</a>
+  </div>`}`;
 
 export function updateNav() {
-    const user = getUserData()
+  const user = getUserData();
 
-    render(navTemplate(user), nav);
+  render(navTemplate(user), nav);
 }
 
-
 function onLogout() {
-    logout()
-    updateNav()
-    page.redirect('/')
+  logout();
+  updateNav();
+  page.redirect("/");
 }
