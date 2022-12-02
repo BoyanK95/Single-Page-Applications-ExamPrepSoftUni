cee9fe4532a9922771f431873c8e43581e12f25b
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-02 02:14:03 +0200

    Finished detailsView

--- ExamPrepGamePlay/src/app.js
+++ ExamPrepGamePlay/src/app.js
@@ -22,7 +22,7 @@ page('/login', showLogin)
 page('/register', showRegister)
 page('/catalog', showCatalog)
 page('/create', showCreate)
-page('/details/:id', ()=> console.log('details'))
+page('/details/:id', showDetails)
 page('/edit/:id', ()=> console.log('edit'))
 page('/search', ()=> console.log('search'))
 
--- ExamPrepGamePlay/src/views/details.js
+++ ExamPrepGamePlay/src/views/details.js
@@ -1,51 +1,53 @@
 import { deleteById, getById } from '../api/data.js'
 import { html, nothing } from '../lib.js'
 
-const detailsTemplate = (item, isOwner, onDelete) => html`
-        <section id="detailsPage">
-            <div class="wrapper">
-                <div class="albumCover">
-                    <img src=${item.imgUrl}>
-                </div>
-                <div class="albumInfo">
-                    <div class="albumText">
+const detailsTemplate = (item, isOwner, hasUser, onDelete) => html`
+        <section id="game-details">
+            <h1>Game Details</h1>
+            <div class="info-section">
 
-                        <h1>Name: ${item.name}</h1>
-                        <h3>Artist: ${item.artist}</h3>
-                        <h4>Genre: ${item.genre}</h4>
-                        <h4>Price: ${item.price}</h4>
-                        <h4>Date: ${item.releaseDate}</h4>
-                        <p>Description: ${item.description}</p>
-                    </div>
-
-                          <!-- Only for registered user and creator of the album-->
-                    ${isOwner ?
-                    html`
-                    <div class="actionBtn">
-                        <a href="/edit/${item._id}" class="edit">Edit</a>
-                        <a @click=${onDelete} href="javascript:void(0)" class="remove">Delete</a>
-                    </div>`:
-                    nothing}
-                  
-                    
+                <div class="game-header">
+                    <img class="game-img" src="images/MineCraft.png" />
+                    <h1>${item.title}</h1>
+                    <span class="levels">MaxLevel: ${item.maxLevel}</span>
+                    <p class="type">${item.category}</p>
                 </div>
+                <p class="text">
+                ${item.summary}
+                </p>
+                <!-- Bonus ( for Guests and Users ) -->
+                <div class="details-comments">
+                    <h2>Comments:</h2>
+                    <ul>
+                        <!-- list all comments for current game (If any) -->
+                        <li class="comment">
+                            <p>Content: I rate this one quite highly.</p>
+                        </li>
+                        <li class="comment">
+                            <p>Content: The best game.</p>
+                        </li>
+                    </ul>
+                    <!-- Display paragraph: If there are no games in the database -->
+                    <p class="no-comment">No comments.</p>
+                </div>
+                 <!-- Edit/Delete buttons ( Only for creator of this game )  -->
+                ${isOwner ? html`<div class="buttons">
+                    <a href="/edit/${item._id}" class="button">Edit</a>
+                    <a @click=${onDelete} href="javascript:void(0)" class="button">Delete</a>
+                </div>` : nothing}
             </div>
+            <!-- Bonus -->
+            <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) -->
+            ${hasUser && !isOwner ? html`
+            <article class="create-comment">
+                <label>Add new comment:</label>
+                <form class="form">
+                    <textarea name="comment" placeholder="Comment......"></textarea>
+                    <input class="btn submit" type="submit" value="Add Comment">
+                </form>
+            </article>` : nothing}
         </section>`
 
-function itemControls(item, hasUser, onDelete) {
-    if (hasUser == false) {
-        return nothing
-    }
-
-    if (isOwner) {
-        return html`
-         <div class="actionBtn">
-            <a href="/edit/${item._id}" class="edit">Edit</a>
-            <a @click=${onDelete} href="javascript:void(0)" class="remove">Delete</a>
-        </div>`
-    }
-
-}
 
 
 export async function showDetails(ctx) {
@@ -55,7 +57,7 @@ export async function showDetails(ctx) {
     const hasUser = Boolean(ctx.user)
     const isOwner = hasUser && ctx.user._id == item._ownerId
     
-    ctx.render(detailsTemplate(item,isOwner, onDelete))
+    ctx.render(detailsTemplate(item,isOwner, hasUser, onDelete))
 
     async function onDelete() {
         const choice = confirm('Are you sure you want to delete this')
